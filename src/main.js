ab66ba6cd900e1de58317de30fb7783c13efc30e
gracri <chrisgraff@protonmail.com>
2018-09-26 14:42:08 +0200

    firebase auth and sign-up

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+var config = {
+  //add your apiKey here
+  apiKey: "",
+  authDomain: "fir-authtutorial-d21cc.firebaseapp.com",
+  databaseURL: "https://fir-authtutorial-d21cc.firebaseio.com",
+  projectId: "fir-authtutorial-d21cc",
+  storageBucket: "fir-authtutorial-d21cc.appspot.com",
+  messagingSenderId: "307702301677"
+};
+firebase.initializeApp(config);
+
 
 /* eslint-disable no-new */
 new Vue({
